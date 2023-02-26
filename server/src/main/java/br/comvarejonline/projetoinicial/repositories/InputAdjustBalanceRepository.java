0dbbe88aa4301553bac683541c65433b87df3ad0
Vinicius Pacheco dos Santos <vinicioss9955@gmail.com>
2023-02-26 20:06:57 -0300

    feat: POST /input-adjust-movements

---
+++ server/src/main/java/br/comvarejonline/projetoinicial/repositories/InputAdjustBalanceRepository.java
@@ -0,0 +1,7 @@
+package br.comvarejonline.projetoinicial.repositories;
+
+import br.comvarejonline.projetoinicial.models.InputAdjustBalanceModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InputAdjustBalanceRepository extends JpaRepository<InputAdjustBalanceModel, Long> {
+}
